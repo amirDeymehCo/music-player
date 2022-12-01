309eba1631ea6b18497f90286b8017e74b20525e
amir <amirdeymeh8@gmail.com>
2022-12-01 16:04:43 +0330

    create list db json-server

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
+import axios from "axios";
 import React, { useContext, useEffect, useState } from "react";
 import Header from "./Components/Header";
 import ListPlayer from "./Components/ListPlayer/ListPlayer";
 import ModalMusic from "./Components/ModalMusic";
+import useSWR from "swr";
+
+const fetcher = (url: string) => axios.get(url);
 
 const App = () => {
   const [showModal, setShowModal] = useState(false);
+
+  const { data, error } = useSWR("http://localhost:8000/users", fetcher, {
+    refreshInterval: 1000,
+  });
+
+  console.log(data);
+
+  const handelAddUser = async (event: any) => {
+    event.preventDefault();
+
+    const newData = {
+      name: "navid",
+      age: 4,
+    };
+
+    await axios.post("http://localhost:8000/users", newData);
+  };
+
   return (
     <>
       {showModal && <ModalMusic setShowModal={setShowModal} />}
@@ -18,6 +40,17 @@ const App = () => {
         <Header />
         <ListPlayer />
       </div>
+
+      <form action="#" onSubmit={handelAddUser}>
+        <input type="text" placeholder="name user" />
+        <button>add user</button>
+      </form>
+
+      {data?.data.map((e: any, i: number) => (
+        <h1 style={{ color: "white" }} key={i}>
+          {e.name}
+        </h1>
+      ))}
     </>
   );
 };
